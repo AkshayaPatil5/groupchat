eb39b50b692c9c46e46b03c601aa66ecc7d46e63
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-07-17 14:31:18 +0530

    task4gc

---
+++ groupchattask3.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require("body-parser");
+const app= express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get("/", (req, res) => {
+    fs.readFile('msg.txt',(err,data)=>{
+        if (err){
+            console.log(err);
+        }
+
+    
+        res.send( 
+            `${data}<form action="/"  method= "POST" onSubmit="document.getElementById('username').value=localStorage.getItem('username')">
+                    <input type="text" name="message" id="message" placeholder="message">
+                    <input type = "hidden" name = "username" id="username">
+                    <br />
+                    <button type ="submit">Send</button>
+                </form>`
+                
+        );
+    
+        
+    });
+
+
+})  
+app.post("/", (req, res) => {
+    const { username, message } = req.body;
+    const data = `${username}: ${message}`;
+    
+    fs.appendFile("msg.txt", data, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
